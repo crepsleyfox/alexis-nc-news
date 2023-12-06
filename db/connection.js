e30a128c4450b4b71a252d62b498e35cf63e510e
Your Namecrepsleyfox <you@example.comalexiscrepsleyfox@gmail.com>
2023-12-06 01:28:35 +0000

    fd

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,19 @@
-
-
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
 const config = {};
 
-if (ENV === 'production') {
+if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-
-
-
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool(config);
